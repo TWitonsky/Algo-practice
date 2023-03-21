c888b01802d3bf847260d9513b61ea1f803aa567
TWitonsky <troywitonsky@gmail.com>
2023-03-21 09:55:08 -0600

    Reverse int done

--- algos/reverse_int.js
+++ algos/reverse_int.js
@@ -9,8 +9,19 @@ reverseInt(-12) --> -21
 */
 
 function reverseInt(x){
+  let negative = Math.sign(x);
 
+  Math.abs(x);
+  //puts its to string, splits each part, reverses it, rejoins.
+  let result = x.toString().split('').reverse().join('');
+  //The above wont get the - so need to times it by a -1 or 1, Javascript math.sign does that
+  result * negative;
 
+  return result;
 };
+
+console.log(reverseInt(592)); //--> 295
+
+console.log(reverseInt(-12)); // --> -21
 
 module.exports = {reverseInt};
