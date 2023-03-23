39fe79d100cf3894f43296b970f2bc1f51426798
TWitonsky <troywitonsky@gmail.com>
2023-03-23 09:48:10 -0600

    testing longest prefix

--- __tests__/longest-common-prefix.js
+++ __tests__/longest-common-prefix.js
@@ -1,5 +1,9 @@
 const { longestCommonPrefix } = require('../algos/longest-common-prefix');
 
 describe('longest common prefix tests:', () => {
-  
+  it('it should come up with the longest commen prefix', () => {
+    expect(longestCommonPrefix(["flower","flow","flight"])).toEqual('fl');
+    expect(longestCommonPrefix(["dog","racecar","trash"])).toEqual('');
+  });
+
 });
