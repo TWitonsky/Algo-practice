4a7d5343a105d9c1788b5657eec0c731af51a3e8
TWitonsky <troywitonsky@gmail.com>
2023-03-27 10:03:40 -0600

    Added array to LL

--- algos/arrayToLL.js
+++ algos/arrayToLL.js
@@ -1,10 +1,21 @@
 /* 
 Write an algorithm that converts an array to a linked list. Return the head node. 
 */
-const { Node } = require('../linkedList')
+const { Node, LinkedList } = require('../linkedList');
 
 const arrayToLL = (arr) => {
-  
+  let node, temp;
+  for(let i=a.length-1; i >= 0; i--){
+    if(!node){
+      node = new Node(a[i]);
+    }
+    else {
+      temp = new Node(a[i]);
+      temp.next = node;
+      node = temp;
+    }
+  }
+  return node;
 }
 
 module.exports = { arrayToLL }
