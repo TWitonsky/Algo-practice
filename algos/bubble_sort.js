eb6ea35c00c69932371e2311d4b5a48b81dcf29f
TWitonsky <troywitonsky@gmail.com>
2023-03-29 09:28:04 -0600

    Bubblesort

--- algos/bubble_sort.js
+++ algos/bubble_sort.js
@@ -9,7 +9,24 @@ output: [2, 3, 5, 5, 6, 8, 9]
 */
 
 function bubble_sort(array) {
-  
+  for(var i = 0; i < arr.length; i++){
+    
+    // Last i elements are already in place 
+    for(var j = 0; j < ( arr.length - i -1 ); j++){
+       
+      // Checking if the item at present iteration
+      // is greater than the next iteration
+      if(arr[j] > arr[j+1]){
+         
+        // If the condition is true then swap them
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  // Print the sorted array
+  console.log(arr);
 }
 
 module.exports = { bubble_sort };
